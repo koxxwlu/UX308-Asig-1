7739684abd744b1d4544052b88736e3cffc81fa2
koxxwlu <144965789+koxxwlu@users.noreply.github.com>
2025-02-28 01:41:06 +0000

    add new stuffs

--- Order.js
+++ Order.js
@@ -4,21 +4,71 @@ export class Order {
         WELCOMING: () => {
           let aReturn = [];
           this.stateCur = this.OrderState.RESERVING;
-          aReturn.push("Welcome to Rich's Acton Rapid Test.");
-          aReturn.push("Would you like to reserve a rapid test kit?");
+          aReturn.push("Welcome to the Foodie's House.");
+          aReturn.push("Would you like to order any food? (y/n)");
           return aReturn;
         },
         RESERVING: (sInput) => {
           let aReturn = [];
-          this.isDone = true;
+          this.stateCur = this.OrderState.ITEM_NR;
+          // this.isDone = true;
           if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-            let d = new Date();
-            d.setMinutes(d.getMinutes() + 120);
-            aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+            aReturn.push('Please type the item number you want: (1/2/3)');
+            aReturn.push('1. Cheese Burger');
+            aReturn.push('2. Sandwich w/ Bacon');
+            aReturn.push('3. Salad & Veggies');
+
+            // if (sInput == 1) {
+            //   aReturn.push('1. elle day')
+
+            //   aReturn.push(`Your food order is reserved under the phone number ${this.sFrom}`);
+            // let d = new Date();
+            // d.setMinutes(d.getMinutes() + 120);
+            // aReturn.push(`Please pick it up at 123 Dalhousie St., Acton before ${d.toTimeString()}`);
+            // }
           } else {
             aReturn.push("Thanks for trying our reservation system");
             aReturn.push("Maybe next time")
+          }
+          return aReturn;
+        },
+        ITEM_NR: (sInput) => {
+          let aReturn = [];
+          if (sInput == 1) {
+            aReturn.push("You've chosen 1. Cheese Burger!")
+            this.stateCur = this.OrderState.UP_SELL;
+          } else if (sInput == 2) {
+            aReturn.push("You've chosen 2. Sandwich w/ Bacon!")
+            this.stateCur = this.OrderState.UP_SELL;
+          } else if (sInput == 3) {
+            aReturn.push("You've chosen 3. Salad & Veggies!")
+            this.stateCur = this.OrderState.UP_SELL;
+          } else {
+            aReturn.push("Sorry, your item isn't recognised. Please enter again.");
+            aReturn.push('Please type the item number you want: (1/2/3)');
+            aReturn.push('1. Cheese Burger');
+            aReturn.push('2. Sandwich w/ Bacon');
+            aReturn.push('3. Salad & Veggies');
+          }
+          aReturn.push("Great! Would you like to make it a set for $3.99? (y/n)")
+          return aReturn;
+        },
+        UP_SELL: (sInput) => {
+          let aReturn = [];
+          if (sInput.toLowerCase().startsWith('y')) {
+            aReturn.push("Great choice! Your total is $14.99.")
+            aReturn.push("Would you like to pay-in-full or with installments? (p/i)")
+          } else {
+            aReturn.push("Are you sure? It's only $3.99 :(")
+            aReturn.push("Great! Would you like to make it a set for $3.99? (y/n)")
+          }
+          return aReturn;
+        },
+        PAYMENT: (sInput) => {
+          let aReturn = [];
+          if (sInput.toLowerCase().startsWith('p')) {
+            aReturn.push("Perfect you're paying in full for your $14.99 meal.")
+            aReturn.push("All set, ")
           }
           return aReturn;
         }
--- spec/order_spec.js
+++ spec/order_spec.js
@@ -4,13 +4,13 @@ describe("Tests all stages of an order", function() {
     it("test hello", function() {
         const oOrder = new Order("999-999-9999");
         const aResults = oOrder.handleInput("hello");
-        expect(aResults[0]).toBe("Welcome to Rich's Acton Rapid Test.")
+        expect(aResults[0]).toBe("Welcome to the Foodie's House.")
     });
     it("test yes", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello")
         const aResults = oOrder.handleInput("yes");
-        expect(aResults[0]).toBe("Your rapid test is reserved under the phone number 999-999-9999")
+        expect(aResults[0]).toBe("Please type the item number you want:")//first line of chat output regardless of later lines
     });
     it("test no", function() {
         const oOrder = new Order("999-999-9999");
@@ -19,5 +19,3 @@ describe("Tests all stages of an order", function() {
         expect(aResults[0]).toBe("Thanks for trying our reservation system")
     });
   });
-  
-  
