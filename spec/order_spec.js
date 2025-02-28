4233d8a04c603fdf3ad07dddead8be76ef15f530
koxxwlu <144965789+koxxwlu@users.noreply.github.com>
2025-02-28 02:17:27 +0000

    make the test work

--- spec/order_spec.js
+++ spec/order_spec.js
@@ -2,20 +2,20 @@ import { Order } from '../Order.js'
 
 describe("Tests all stages of an order", function() {
     it("test hello", function() {
-        const oOrder = new Order("999-999-9999");
+        const oOrder = new Order("order one");
         const aResults = oOrder.handleInput("hello");
-        expect(aResults[0]).toBe("Welcome to the Foodie's House.")
+        expect(aResults[0]).toBe("Welcome to the Foodie's House.");
     });
     it("test yes", function() {
-        const oOrder = new Order("999-999-9999");
-        oOrder.handleInput("hello")
+        const oOrder = new Order("order two");
+        oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("yes");
-        expect(aResults[0]).toBe("Please type the item number you want:")//first line of chat output regardless of later lines
+        expect(aResults[0]).toBe("Please type the item number you want: (1/2/3)");//first line of chat output regardless of later lines
     });
     it("test no", function() {
-        const oOrder = new Order("999-999-9999");
-        oOrder.handleInput("hello")
+        const oOrder = new Order("order three");
+        oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("no");
-        expect(aResults[0]).toBe("Thanks for trying our reservation system")
+        expect(aResults[0]).toBe("Thanks for trying our reservation system, come again!");
     });
   });
